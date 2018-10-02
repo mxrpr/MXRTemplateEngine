e1fa19c1c678b9677f12c36434cfe215f6acabcc
ferenc.tollas <ferenc.tollas@gmail.com>
2018-10-02 11:18:15 +0200

    MXRTemplateEngine: implement buildAST function

--- src/main/kotlin/com/mxr/template/MXRTemplateEngine.kt
+++ src/main/kotlin/com/mxr/template/MXRTemplateEngine.kt
@@ -2,6 +2,7 @@ package com.mxr.template
 
 import com.mxr.template.nodes.*
 import java.io.File
+import java.util.*
 
 /**
  * From an input string or file parses the
@@ -46,7 +47,7 @@ class MXRTemplateEngine(content: String) {
     }
 
     private fun parseTokens(tokens: Array<String>): Array<Token> {
-        var result = mutableListOf<Token>()
+        val result = mutableListOf<Token>()
         for (token in tokens) {
             if (token.startsWith("{{#")) {
                 result.add(Token(token, TokenType.OPEN_BLOCK_TOKEN))
@@ -72,7 +73,7 @@ class MXRTemplateEngine(content: String) {
         return result.toTypedArray()
     }
 
-    fun createNodeFromToken(token: Token): Node {
+    private fun createNodeFromToken(token: Token): Node {
         if (token.tokenType == TokenType.TEXT_TOKEN)
             return TextNode(token.textFragment)
 
@@ -106,12 +107,26 @@ class MXRTemplateEngine(content: String) {
 
     private fun buildAST(tokens: Array<Token>): RootNode {
         val rootNode = RootNode()
-        val scopeStack = mutableListOf<Node>()
+        val scopeStack = Stack<Node>()
+        scopeStack.push(rootNode)
+
         for (token in tokens) {
             if (scopeStack.isEmpty())
                 throw Exception("Too many end tags or something is not closed correctly: '$token'")
-            val parentScope = scopeStack.last()
+            val parentNode = scopeStack.peek()
+            if (token.tokenType == TokenType.CLOSE_BLOCK_TOKEN) {
+                // pop last element from stack
+                scopeStack.pop()
+                // check for end tags
+            }
+            else {
+                val node = this.createNodeFromToken(token)
+                parentNode.childrens.add(node)
+                if (node.hasScope)
+                    scopeStack.push(node)
+            }
         }
+
         return rootNode
     }
 
@@ -119,10 +134,10 @@ class MXRTemplateEngine(content: String) {
         val result = mutableListOf<String>()
         val regexp = REGEXP.toRegex()
         val sequence = regexp.findAll(content)
-        for (match in sequence) {
+
+        for (match in sequence)
             result.add(match.value)
-            println("-> ${match.value}")
-        }
+
         return result.toTypedArray()
     }
 }
